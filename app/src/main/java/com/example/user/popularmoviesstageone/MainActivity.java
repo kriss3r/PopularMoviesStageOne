3055d4a69f9e73542283fb6fdbdfcd63d6356bb4
pulsedriv <rymarowicz@gmail.com>
2017-04-05 17:40:11 +0200

    feat: options menu added

--- app/src/main/java/com/example/user/popularmoviesstageone/MainActivity.java
+++ app/src/main/java/com/example/user/popularmoviesstageone/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.user.popularmoviesstageone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+ // used to link main_menu.xml as options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+// used to get info about which sort option to use
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()==R.id.it_most_popular){
+            Log.i("i","most popular");
+        }else {
+            Log.i("i","top rated");
+        }
+       // Log.i("ERROR", item.
+        return super.onOptionsItemSelected(item);
     }
 }
